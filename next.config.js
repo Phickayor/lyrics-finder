911b680809a0c5324ce4b9d6cd342c9b542dbaee
Fikayo Jetawo <jetawof@gmail.com>
2023-05-09 22:28:56 +0100

    adding keys

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    apiKey: 'a4a4238bd7msh9bfb6185442f113p1a3a65jsna92831658d49',
+    apiHost: 'genius-song-lyrics1.p.rapidapi.com',
+  },
   reactStrictMode: true,
 }
 
