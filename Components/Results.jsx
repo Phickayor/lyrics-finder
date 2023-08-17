72c7b134b131008f4e9c1ad8bb4dc8efde627373
Fikayo Jetawo <jetawof@gmail.com>
2023-08-17 01:16:21 +0100

    follow up

--- Components/Results.jsx
+++ Components/Results.jsx
@@ -1,26 +1,40 @@
-import { Poppins } from 'next/font/google';
-import Image from 'next/image';
-import React from 'react'
-const poppins = Poppins({ weight: '900', subsets: ['latin'] })
+import { Poppins } from "next/font/google";
+import React from "react";
+const poppins = Poppins({ weight: "900", subsets: ["latin"] });
 function Results(props) {
-    var searchResults = props.results;
+  var searchResults = props.results;
 
-    return (
-        <div className='grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:py-10 py-2 px-5 text-center'>
-
-            {
-                searchResults.map((results, index) => (
-                    <div key={index} className='p-[30px] w-full'>
-                        <Image width={100} height={100} src={results['result']['header_image_url']} className='rounded-2xl' alt="Album Cover" />
-                        <div className='space-y-3 my-4'>
-                            <h3 className={`text-3xl font-mono font-semibold ${poppins.className}`}>{results.result.title}</h3>
-                            <p className='text-2xl text-slate-400'>{results.result.artist_names}</p>
-                            <a href={results.result.url} className='text-3xl text-[#ff0000]' onClick={()=>{console.log(results['result']['header_image_url'])}>View Lyrics</a>
-                        </div>
-                    </div>
-                ))
-            }
+  return (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:py-10 py-2 px-5 text-center">
+      {searchResults.map((results, index) => (
+        <div key={index} className="p-[30px] w-full">
+          <img
+            src={results["result"]["header_image_url"]}
+            className="rounded-2xl"
+            alt="Album Cover"
+          />
+          <div className="space-y-3 my-4">
+            <h3
+              className={`text-3xl font-mono font-semibold ${poppins.className}`}
+            >
+              {results.result.title}
+            </h3>
+            <p className="text-2xl text-slate-400">
+              {results.result.artist_names}
+            </p>
+            <a
+              href={results.result.url}
+              className="text-3xl text-[#ff0000]"
+              onClick={() => {
+                console.log(results["result"]["header_image_url"]);
+              }}
+            >
+              View Lyrics
+            </a>
+          </div>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
-export default Results
+export default Results;
--- Components/Search.jsx
+++ Components/Search.jsx
@@ -1,39 +1,58 @@
-import React, { useRef } from 'react'
-import { Krub, Poppins } from 'next/font/google';
-const krub = Krub({ weight: '500', subsets: ['latin'] })
-const poppins = Poppins({ weight: '900', subsets: ['latin'] })
+import React, { useRef } from "react";
+import { Krub, Poppins } from "next/font/google";
+const krub = Krub({ weight: "500", subsets: ["latin"] });
+const poppins = Poppins({ weight: "900", subsets: ["latin"] });
 function Search(props) {
-    const searchContainer = useRef(null)
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'a4a4238bd7msh9bfb6185442f113p1a3a65jsna92831658d49',
-            'X-RapidAPI-Host': "genius-song-lyrics1.p.rapidapi.com"
-        }
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        Result(searchContainer.current.value)
+  const searchContainer = useRef(null);
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "a4a4238bd7msh9bfb6185442f113p1a3a65jsna92831658d49",
+      "X-RapidAPI-Host": "genius-song-lyrics1.p.rapidapi.com"
     }
-    function Result(search) {
-        fetch(`https://genius-song-lyrics1.p.rapidapi.com/search/?q=${search}&per_page=12&page=1`, options).then(function (response) {
-            return response.json()
-        }).then(function (data) {
-            props.resultHandler(data.hits)
-        })
-    }
-    return (
-        <div className='text-white mx-auto lg:w-9/12 w-11/12'>
-            <h1 className={`${poppins.className} lg:text-8xl text-5xl pb-10 py-20 text-center font-semibold`}><b className='text-[#ff0000] font-semibold'>Lyrics</b> Finder</h1>
-            <div className={`${krub.className} flex flex-col`}>
-                <p className='lg:text-5xl text-3xl text-center text-slate-400'>Search for any song lyrics on the spot.</p>
-                <input type='search' ref={searchContainer} className='px-10 mt-10 py-4 lg:py-8 bg-[#0d1117] outline-none text-center font-semibold text-3xl rounded-full w-full object-center' />
-                <button className='bg-[#ff0000] font-semibold text-xl my-5 rounded-full w-8/12 lg:w-6/12 py-3 lg:py-5 self-center' onClick={handleSubmit}>Search</button>
-            </div>
-
-
-        </div>
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    Result(searchContainer.current.value);
+  };
+  function Result(search) {
+    fetch(
+      `https://genius-song-lyrics1.p.rapidapi.com/search/?q=${search}&per_page=12&page=1`,
+      options
     )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        props.resultHandler(data.hits);
+      });
+  }
+  return (
+    <div className="text-white mx-auto lg:w-9/12 w-11/12">
+      <h1
+        className={`${poppins.className} lg:text-8xl text-5xl pb-10 py-20 text-center font-semibold`}
+      >
+        <b className="text-[#ff0000] font-semibold">Lyrics</b> Finder
+      </h1>
+      <div className={`${krub.className} flex flex-col`}>
+        <p className="lg:text-5xl text-3xl text-center text-slate-400">
+          Search for any song lyrics on the spot.
+        </p>
+        <input
+          type="search"
+          ref={searchContainer}
+          className="px-10 mt-10 py-4 lg:py-8 bg-[#0d1117] outline-none text-center font-semibold text-3xl rounded-full w-full object-center"
+        />
+        <button
+          type="submit"
+          className="bg-[#ff0000] font-semibold text-xl my-5 rounded-full w-8/12 lg:w-6/12 py-3 lg:py-5 self-center"
+          onClick={handleSubmit}
+        >
+          Search
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Search 
+export default Search;
